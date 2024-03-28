8193f7ffc008fb498cee2285239fdc655654443d
Shifa <shifa.gadiwale@gmail.com>
2024-03-29 00:58:35 +0530

    Counter Implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = (num)=>{
+    setCount((prevCount)=>prevCount+num);
+  }
+
   return (
     <div className="App">
-      
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button onClick={()=>{handleClick(1)}}>Increment</button>
+      <button onClick={()=>{handleClick(-1)}}>Decrement</button>
+
     </div>
   );
 }
